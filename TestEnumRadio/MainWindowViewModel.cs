b21556ce7af223c53f6ac167e82b05cadb517692
agatino.work@gmail.com <agatino.work@gmail.com>
2020-05-22 13:39:13 +0200

    Add project files.

---
+++ TestEnumRadio/MainWindowViewModel.cs
@@ -0,0 +1,25 @@
+﻿using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+
+namespace TestEnumRadio
+{
+    public enum Numbers { Uno, Due, Tre}
+		
+    public class MainWindowViewModel : ViewModelBase
+    {
+        private RelayCommand _showValueCmd;
+        public RelayCommand ShowValueCmd => _showValueCmd ?? (_showValueCmd = new RelayCommand(
+                () => MessageBox.Show(Number.ToString()),
+                () => { return 1 == 1; },
+                keepTargetAlive: true
+                ));
+
+        private Numbers _number = Numbers.Uno;
+        public Numbers Number { get => _number; set { Set(() => Number, ref _number, value); }}
+    }
+}
---
+++ TestEnumRadio/SmartConverterEnum.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Windows.Data;
+using System.Windows.Markup;
+
+namespace TestEnumRadio
+{
+    public class SmartConverterEnum : MarkupExtension, IValueConverter
+    {
+
+        public object ValueTrue { get; set; }
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value.Equals(parameter);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if ((bool)value)
+                return parameter;
+            else
+                return Binding.DoNothing;
+        }
+
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return this;
+        }
+    }
+
+}
+
